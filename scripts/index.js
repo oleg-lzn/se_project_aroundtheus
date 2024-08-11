de45d1fe977f36ed70068e730271c36e7d106856
Oleg <luzeninoleg2012@gmail.com>
2024-08-11 13:47:27 +0300

    adding the array

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+let yosemiteValley = {
+  name: "yosemiteValley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
+
+let lakeLouise = {
+  name: "Lake Louise",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+};
+
+let baldMountains = {
+  name: "Bald Mountains",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+};
+
+let latemar = {
+  name: "Latemar",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+};
+
+let vanoiseNationalPark = {
+  name: "Vanoise National Park",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+};
+
+let lagoDiBraies = {
+  name: "Lago di Braies",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+};
+
+let initialCards = [
+  yosemiteValley,
+  lakeLouise,
+  baldMountains,
+  latemar,
+  vanoiseNationalPark,
+  lagoDiBraies,
+];
